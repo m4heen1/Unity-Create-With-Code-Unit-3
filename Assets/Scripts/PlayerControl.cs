615b546f82b9b4ae029488653a22f144dfa82f32
m4heen1 <bhuim263@hsd.k12.or.us>
2025-05-13 20:00:29 -0700

    First Commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpforce = 10;
+    public float gravityModifer;
+    public bool is Onground = true 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifer;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space) && isOnground) {
+        playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        isOnground = false;
+      }  
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnground = true; 
+    }
+}
+
